5150023cad3357976983356cdf3aaf085d65a2e8
bartavelle <bartavelle@gmail.com>
2012-01-23 17:25:18 +0100

    activation lsyslog

--- manifests/distro/lucid.pp
+++ manifests/distro/lucid.pp
@@ -4,6 +4,7 @@ class mysql_proxy::distro::lucid
     pinning { 'mysql-proxy-natty': packages => ['mysql-proxy','libglib2.0-0','libpcre3'], distro => 'natty'; }
     file {
         '/etc/init/mysql-proxy.conf': source => 'puppet:///modules/mysql_proxy/mysql-proxy.conf';
+        '/usr/lib/mysql-proxy/lua/lsyslog.so': source => 'puppet:///modules/mysql_proxy/lsyslog.so', owner => 'root', group => 'root', mode => '755', require => Package['mysql-proxy'];
         '/etc/init.d/mysql-proxy': ensure => absent;
     }
 }
